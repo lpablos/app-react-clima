42a8cea586360b20254033d8661610d82327e07d
Luis Jorge P.S <lpablo@toshiba.com>
2021-07-15 01:02:35 -0500

    peticion a la api y configuracion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
 const App = () => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: 'MX'
+
+  })
+
+  const [consultar, setConsultar] = useState(false)
+  
+  const {ciudad, pais} = busqueda
+
+  useEffect(() => {
+    if(consultar){
+      const consultarAPI = async ()=>{
+        const appId = '26c1084d972c52760dea736e66166909'
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+        const respuesta = await fetch(url)
+                          .then(peticcion=> peticcion.json())
+        console.log("PEticion", respuesta);
+      }
+      consultarAPI()
+    }
+    
+  }, [ciudad, consultar, pais])
   return (
     <Fragment>
       <Header titulo="Clima React App"/>
       <div className="contenedor-form">
         <div className="row">
           <div className="col m6 s12">
-            <Formulario/>
+            <Formulario
+            busqueda={busqueda}
+            setBusqueda={setBusqueda}
+            setConsultar={setConsultar}/>
           </div>
           <div className="col m6 s12">
             2
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,7 @@
 import React, {Fragment, useState} from 'react'
 
-const Formulario = () => {
-    const [busqueda, setBusqueda] = useState({
-        ciudad: '',
-        pais: 'MX'
-
-    })
+const Formulario = ({busqueda, setBusqueda, setConsultar}) => {
+    
     // Fragmentar a busqueda para el value
     const { ciudad, pais} = busqueda;
 
@@ -16,10 +12,30 @@ const Formulario = () => {
             [e.target.name] : e.target.value
         })
     }
+
+    const [error, setError] = useState(false)
+
+    const handleSubmit = e =>{
+        
+        e.preventDefault()
+        // Validar
+        if(ciudad.trim()==='' || pais.trim() === ''){
+            setError(true)
+            return
+        }
+        setError(false)
+        // Pasarlo al componente principal
+        setConsultar(true)
+    }
     return (
         <Fragment>
             <h1>Formulario</h1>
-            <form action="" method="post">
+            <form onSubmit={handleSubmit} method="post">
+                {
+                error 
+                ? <p className="red darken-4 error">Todos los campos son obligatorios</p> 
+                : null
+                }
                 <div className="input-field col s12">
                     <input type="text" 
                         name="ciudad" 
@@ -43,6 +59,11 @@ const Formulario = () => {
                         <option value="PE">Perú</option>
                     </select>
                     <label htmlFor="pais">Pais</label>
+                </div>
+                <div className="input-field col s12">
+                    <button className="waves-effect waves-light btn-large btn-block yellow accent-4" type="submit">
+                        Buscar Clima
+                    </button>
                 </div>
             </form>
         </Fragment>
